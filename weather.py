157c496011ab2f5c431a9a813c9c88a4f197e026
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2024-06-22 20:17:45 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ from googletrans import Translator
 
 def weather_city(city):
   try:
+    translator = Translator()
     itogi = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={da}&appid=adbb2821feabdbc9583b52bec964c8c1').json()
     city = translator.translate(itogi['name'], dest="ru")
     weather = translator.translate(itogi['weather'][0]['description'], dest="ru")
