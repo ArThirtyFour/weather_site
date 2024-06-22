7d11ecbd656a6b27c33b1203c08d32704509742c
AR34 <149522975+ArThirtyFour@users.noreply.github.com>
2024-06-22 20:20:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import requests
 from flask import Flask , render_template ,request
-from googletrans import Translator
+from weather import weather_city
 
 app = Flask(__name__)
-translator = Translator()
 
 @app.route('/')
 def da1():
@@ -12,14 +10,11 @@ def da1():
 
 @app.route('/result',methods=['GET','POST'])
 def da2():
-    da= request.form['city'].lower()
-    itogi = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={da}&appid=adbb2821feabdbc9583b52bec964c8c1').json()
-    try:
-        city = translator.translate(itogi['name'], dest="ru")
-        weather = translator.translate(itogi['weather'][0]['description'], dest="ru")
-        temperature = round(float(itogi['main']['temp'] - 273.0 ),2)
-        return render_template('daa.html',json_da=city.text , weather=weather.text[0]+weather.text[1:], tempe=temperature)
-    except:
+    da = request.form['city'].lower()
+    if weather_city(da):
+        city , weather , temperature = weather_city(da)
+        return render_template('daa.html',json_da=city, weather=weather , tempe=temperature)
+    else:
         return 'NOT VALID'
 
 if __name__ == '__main__':
